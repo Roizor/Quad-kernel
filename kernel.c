603a3e2857b4abe4e530c853ac3bddd45bcfb2d3
TFTWPhoenix <chucktcg12@gmail.com>
2021-01-09 19:23:06 -0600

    Maek better

--- kernel.c
+++ kernel.c
@@ -1,12 +1,10 @@
+#include "libs/iknow/screen.h"
+
 init()
 {
 
-        char* vidmem=(char*)0xb8000;
-        vidmem[0] = 'S';
-        vidmem[1] = 0x0f;
-        vidmem[2] = 'a';
-        vidmem[3] = 0x0f;        
-        vidmem[4] = 'x';
-        vidmem[5] = 0x0f;
+        clearScreen();
+        print("Sax Kernel\n");
+        print("By SaxNBT and TFTWPhoenix\n");
 
 }
