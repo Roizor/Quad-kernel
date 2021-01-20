0717e00347ac07ab681f9650b6d4eed3add6d508
Roizor <68617740+Roizor@users.noreply.github.com>
2021-01-20 15:25:07 -0600

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -7,7 +7,9 @@ section         .text
         
 global start
 extern init            ; this function is gonna be located in our c code(kernel.c)
+extern advertise        ; yum
 start:
         cli             ;clears the interrupts 
         call init      ;send processor to continue execution from the kamin funtion in c code
+        call advertise
         hlt             ; halt the cpu(pause it from executing from this address
